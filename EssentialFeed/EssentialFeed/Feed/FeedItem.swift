f4f1f02eb27acd2d4d6f298bb5ddc2c3993ccec0
Jastin Martinez <martinez.jastin@hotmail.com>
2022-07-30 17:08:24 -0400

    Initial structure
    init single model with protocol to be implemented

---
+++ EssentialFeed/EssentialFeed/Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jastin on 30/7/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
