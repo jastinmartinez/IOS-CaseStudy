997939336986b3e812c88ac640306d0342fbd216
Jastin Martinez <martinez.jastin@hotmail.com>
2022-07-30 18:18:21 -0400

    refactor test to remove duplication

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -8,41 +8,56 @@
 import XCTest
 @testable import EssentialFeed
 
-class RemoteFeedLoaderTest: XCTestCase {
-
-    func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
-        XCTAssertNil(client.requestedUrl)
-    }
-    
-    func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
-        sut.load()
-        XCTAssertNotNil(client.requestedUrl)
-    }
-}
-
-
-class RemoteFeedLoader {
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    func load() {
-        client.get(from: URL(string: "url")!)
-    }
-}
-
 protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedUrl: URL?
-     func get(from url: URL) {
-        requestedUrl = url
+class RemoteFeedLoader {
+     let client: HTTPClient
+     let url: URL
+    init(url: URL,client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    func load() {
+        client.get(from: self.url)
     }
 }
+
+class RemoteFeedLoaderTest: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+     let (_, client) = makeSUT()
+        XCTAssertNil(client.requestedUrl)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "given-url-load")!
+        let(sut, client) = makeSUT(url: url)
+        sut.load()
+        XCTAssertEqual(sut.url, client.requestedUrl)
+    }
+    
+    // MARK: - HELPERS
+    
+    
+    private func makeSUT(url: URL = URL(string: "given-url")!) -> (RemoteFeedLoader,HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url,client: client)
+        return (sut,client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedUrl: URL?
+         func get(from url: URL) {
+            requestedUrl = url
+        }
+    }
+}
+
+
+
+
+
+
+
