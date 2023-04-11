60282678b81c1ceaa36166c00a1e0089d74286b8
Jastin Martinez <martinez.jastin@hotmail.com>
2023-04-10 23:43:06 -0400

    objc initial chapter 1

---
+++ Objective-C/BasicSyntax/BasicSyntax/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  BasicSyntax
+//
+//  Created by Jastin on 4/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        // ojc.c is not type inference
+        const int i = 10;
+        // objects needs special notation *(it's called pointer) when declare as variable.
+        NSString *str = @"Demo";
+        
+        //arrays
+        NSArray *array = @[@"Demo", @"Demo1"];
+        NSLog(@"srt");
+    }
+    return 0;
+}
