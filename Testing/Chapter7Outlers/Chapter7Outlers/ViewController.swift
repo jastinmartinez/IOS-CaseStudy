e278adb3ec28cc7e88f987ee1d5f0a9bf1eb2ae8
Jastin Martinez <martinez.jastin@hotmail.com>
2023-07-23 19:20:00 -0400

    init repository of testing outlets

---
+++ Testing/Chapter7Outlers/Chapter7Outlers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter7Outlers
+//
+//  Created by Jastin on 23/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
