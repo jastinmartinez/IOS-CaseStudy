eec6e75cdb8cb39d05d9b76504cfd3df8962d32c
Jastin Martinez <martinez.jastin@hotmail.com>
2023-07-23 19:20:19 -0400

    init new xib viewcontroller to handle outlets connections

---
+++ Testing/Chapter7Outlers/Chapter7Outlers/OutletViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OutletViewController.swift
+//  Chapter7Outlers
+//
+//  Created by Jastin on 23/7/23.
+//
+
+import UIKit
+
+class OutletViewController: UIViewController {
+    
+    @IBOutlet private(set) weak var label1: UILabel!
+    @IBOutlet private(set) weak var label2: UILabel!
+    @IBOutlet private(set) weak var stack1: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
