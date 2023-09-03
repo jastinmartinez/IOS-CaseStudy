4b3d7168c9420df33e568a7ff69906d63db18d97
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-03 14:16:03 -0400

    fix end of chapter

--- Testing/Chap10NavigationBetweenScreen/Chap10NavigationBetweenScreenTests/Chap10NavigationBetweenScreenTests.swift
+++ Testing/Chap10NavigationBetweenScreen/Chap10NavigationBetweenScreenTests/Chap10NavigationBetweenScreenTests.swift
@@ -15,39 +15,46 @@ final class Chap10NavigationBetweenScreenTests: XCTestCase {
     
     override func setUp() {
         self.setSutAndLoadViewIfNeed()
+        super.setUp()
     }
     
     override func tearDown() {
         self.sut = nil
+        self.nav = nil
         super.tearDown()
     }
     
     func test_pushNavigationFromCode_ReturnCodeNextViewController() {
         self.tap(self.sut.codePushButton)
+        self.executeRunLoop()
         XCTAssertNotNil(sut.navigationController?.viewControllers.last as? CodeNextViewController)
     }
     
     func test_pushNavigationFromCodePassValue_ShouldReturnCCodeNextViewControllerWithValue() {
         self.tap(self.sut.codePushButton)
+        self.executeRunLoop()
         let codeNextViewController = sut.navigationController?.viewControllers.last as? CodeNextViewController
         XCTAssertEqual(codeNextViewController?.label.text, "Push from code")
     }
     
     func test_pushViewControllerFromCode_ShouldReturnCodeNextViewControllerAnimated() {
         self.tap(self.sut.codePushButton)
-        XCTAssertTrue(self.nav.pushViewControllerAnimatedList.count > 0)
+        self.executeRunLoop()
+        XCTAssertTrue(self.nav.pushViewControllerAnimatedList.count == 2)
     }
     
-    private func navigationControllerFactory() -> SpyNavigationController {
-        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
-        let sut: UINavigationController = mainStoryBoard.instantiateViewController(identifier:  "NavController")
-        let spyNavigationController = SpyNavigationController(rootViewController: sut.topViewController!)
+    private func navigationControllerFactory() -> SpyNavigationController! {
+        let nav: UINavigationController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "NavController")
+        guard let viewController = nav.topViewController as? ViewController else {
+            XCTFail("the top viewcontroller is not the expected")
+            return nil
+        }
+        let spyNavigationController = SpyNavigationController(rootViewController: viewController)
         return spyNavigationController
     }
     
     private func tap(_ button: UIButton) {
         button.sendActions(for: .touchUpInside)
-        self.executeRunLoop()
     }
     
     private func setSutAndLoadViewIfNeed() {
@@ -59,12 +66,13 @@ final class Chap10NavigationBetweenScreenTests: XCTestCase {
     private func executeRunLoop() {
         RunLoop.current.run(until: Date())
     }
-    
-    private class SpyNavigationController: UINavigationController {
-        private (set) var pushViewControllerAnimatedList = [Bool]()
-        override func pushViewController(_ viewController: UIViewController, animated: Bool) {
-            super.pushViewController(viewController, animated: animated)
-            self.pushViewControllerAnimatedList.append(animated)
-        }
+}
+
+private final class SpyNavigationController: UINavigationController {
+    private (set) var pushViewControllerAnimatedList = [Bool]()
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        super.pushViewController(viewController, animated: animated)
+        self.pushViewControllerAnimatedList.append(animated)
     }
 }
+
