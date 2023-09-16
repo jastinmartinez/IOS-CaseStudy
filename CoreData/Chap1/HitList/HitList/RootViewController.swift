9161f35745bc75444e8dc1400d271000d46b10f8
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-16 14:15:01 -0400

    Set up hitlistTableView

--- CoreData/Chap1/HitList/HitList/RootViewController.swift
+++ CoreData/Chap1/HitList/HitList/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  HitList
 //
 //  Created by Jastin on 16/9/23.
@@ -8,12 +8,24 @@
 import UIKit
 
 class RootViewController: UIViewController {
-
+    
+    private let hitistTableView: UITableView = {
+        let xTableView = UITableView()
+        xTableView.backgroundColor = .white
+        xTableView.translatesAutoresizingMaskIntoConstraints = false
+        return xTableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.onCreate()
     }
-
-
+    
+    private func onCreate() {
+        self.view.addSubview(self.hitistTableView)
+        NSLayoutConstraint.activate([self.hitistTableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                                     self.hitistTableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+                                     self.hitistTableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+                                     self.hitistTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)])
+    }
 }
-
