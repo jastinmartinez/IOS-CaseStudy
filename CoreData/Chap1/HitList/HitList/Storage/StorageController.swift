1eca0d4f439a4545cc48481a3ec0b81f1a46d340
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-16 18:53:42 -0400

    create `storageController` a class that handler storage

---
+++ CoreData/Chap1/HitList/HitList/Storage/StorageController.swift
@@ -0,0 +1,14 @@
+//
+//  StorageController.swift
+//  HitList
+//
+//  Created by Jastin on 16/9/23.
+//
+
+import Foundation
+import CoreData
+
+final class StorageController {
+    
+    private var people = [NSManagedObject]()
+}
