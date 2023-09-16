665c8eaf2f1a6452dfa9ac1a5cb77f1152b63c36
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-16 14:12:39 -0400

    init set rootViewController a navigationviewcontroller, remove some storyboard references and start project manually

---
+++ CoreData/Chap1/HitList/HitList/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Jastin on 16/9/23.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
