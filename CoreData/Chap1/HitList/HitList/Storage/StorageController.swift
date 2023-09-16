95d21f8f721aec37a3600560cbac435c12495212
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-16 19:26:10 -0400

    create `StorageController` class that handle `CoreData` implementation, include a protocol to expose funcionalities

--- CoreData/Chap1/HitList/HitList/Storage/StorageController.swift
+++ CoreData/Chap1/HitList/HitList/Storage/StorageController.swift
@@ -8,7 +8,58 @@
 import Foundation
 import CoreData
 
-final class StorageController {
-    
+protocol StorageControllerProtocol: AnyObject {
+    func save(name: String)
+    func get() -> [String]
+}
+
+final class StorageController: StorageControllerProtocol {
+  
     private var people = [NSManagedObject]()
+    private let persistentContainer: NSPersistentContainer
+    
+    init() {
+        self.persistentContainer = NSPersistentContainer(name: Constant.contaniner.rawValue)
+        self.persistentContainer.loadPersistentStores { storeDescription, error in
+            if let error = error as? NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    
+    private func buildPerson(name: String,
+                             entity: NSEntityDescription,
+                             manageObjectContext: NSManagedObjectContext) -> NSManagedObject {
+        let person = NSManagedObject(entity: entity, insertInto: manageObjectContext)
+        person.setValue(name, forKeyPath: Constant.name.rawValue)
+        return person
+    }
+    
+    func save(name: String) {
+        do {
+            let manageObjectContext = self.persistentContainer.viewContext
+            guard let entity = NSEntityDescription.entity(forEntityName: Constant.entity.rawValue, in: manageObjectContext) else {
+                return
+            }
+            let person = buildPerson(name: name,
+                                     entity: entity,
+                                     manageObjectContext: manageObjectContext)
+            try self.persistentContainer.viewContext.save()
+            self.people.append(person)
+        } catch let error as NSError {
+            fatalError("could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func get() -> [String] {
+        return self.people.lazy.compactMap({ $0.value(forKey: Constant.name.rawValue) as? String})
+    }
+}
+
+extension StorageController {
+    private enum Constant: String {
+        case contaniner = "HitList"
+        case entity = "Person"
+        case name
+    }
 }
