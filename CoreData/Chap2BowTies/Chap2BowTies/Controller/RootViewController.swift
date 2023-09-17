48e23b043ba850a56912ca71c060f85c9b3d3fa3
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-17 19:01:40 -0400

    include initial UI components

--- CoreData/Chap2BowTies/Chap2BowTies/Controller/RootViewController.swift
+++ CoreData/Chap2BowTies/Chap2BowTies/Controller/RootViewController.swift
@@ -9,6 +9,41 @@ import UIKit
 
 class RootViewController: UIViewController {
     
+    private let bowTieSegmentedControl: UISegmentedControl = {
+        let items = ["R", "O", "Y", "G", "B", "I", "V"]
+        let xSegumentedControl = UISegmentedControl(items: items)
+        xSegumentedControl.translatesAutoresizingMaskIntoConstraints = false
+        return xSegumentedControl
+    }()
+    
+    private let bowImageView: UIImageView = {
+        let xImageView = UIImageView()
+        xImageView.translatesAutoresizingMaskIntoConstraints = false
+        return xImageView
+    }()
+    
+    private lazy var ratingLabel: UILabel = self.labelBuilder(placeHolder: "Rating")
+    private lazy var ratingValueLabel: UILabel = self.labelBuilder(placeHolder: "4/5")
+    private lazy var numberOfTimeLabel: UILabel = self.labelBuilder(placeHolder: "# of times worn:")
+    private lazy var numberOfTimeValueLabel: UILabel = self.labelBuilder(placeHolder: "5")
+    private lazy var lastWornLabel: UILabel = self.labelBuilder(placeHolder: "Last worn:")
+    private lazy var lastWornValueLabel: UILabel = self.labelBuilder(placeHolder: "06/25/14")
+    private lazy var favoriteLabel: UILabel = self.labelBuilder(placeHolder: "*Favorite*")
+    private lazy var bowTieTitleLabel: UILabel = self.labelBuilder(placeHolder: "Bow Tie Name Label",
+                                                                   fontSize: 16,
+                                                                   isBold: true)
+    
+    private func labelBuilder(placeHolder: String,
+                              fontSize: CGFloat = 12.0,
+                              isBold: Bool = false) -> UILabel {
+        let xLabel = UILabel()
+        xLabel.translatesAutoresizingMaskIntoConstraints = false
+        xLabel.text = placeHolder
+        xLabel.font = .systemFont(ofSize: fontSize,
+                                  weight: isBold ? .bold : .black)
+        return xLabel
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.onCreate()
