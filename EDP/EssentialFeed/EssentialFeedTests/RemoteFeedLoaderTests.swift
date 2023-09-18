c7e6708ac3428e16a09845937c9ca20cb89062bb
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-18 13:57:52 -0400

    Add test for Execute `Loadd Feed items` and when given URL is pass from an abstraction HTTPClient

--- EDP/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EDP/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,5 +1,5 @@
 //
-//  EssentialFeedTests.swift
+//  RemoteFeedLoaderTests.swift
 //  EssentialFeedTests
 //
 //  Created by Jastin on 16/9/23.
@@ -8,29 +8,51 @@
 import XCTest
 @testable import EssentialFeed
 
-final class EssentialFeedTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func load() {
+        self.client.get(from: self.url)
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+final class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = self.makeSUT()
+        XCTAssertNil(client.requestedURL)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        XCTAssertEqual(url, client.requestedURL)
+    }
+    
+    //   MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            self.requestedURL = url
         }
     }
-
 }
