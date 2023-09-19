329b6b10c7cd1f7c8d52a96f47e3644603ac25e6
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-19 18:44:58 -0400

    add some attributes to the `bowtie` core data model, include a color data transformer and add set up

---
+++ CoreData/Chap2BowTies/Chap2BowTies/Storage/transformation/ColorAttributeTransformer.swift
@@ -0,0 +1,12 @@
+//
+//  ColorAttributeTransformer.swift
+//  Chap2BowTies
+//
+//  Created by Jastin on 19/9/23.
+//
+
+import UIKit
+
+class ColorAttributeTransformer: NSSecureUnarchiveFromDataTransformer {
+
+}
