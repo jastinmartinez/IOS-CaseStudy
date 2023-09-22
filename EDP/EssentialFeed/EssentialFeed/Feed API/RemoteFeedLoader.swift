cf9f3c27da7363885cb2797cd555ce4edc703f71
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-22 17:21:53 -0400

    Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths

--- EDP/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EDP/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,21 +7,38 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case sucess(HTTPURLResponse)
+    case failure(Error)
+}
+
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
     
     private let url: URL
     private let client: HTTPClient
-   
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    public func load() {
-        self.client.get(from: self.url)
+    public func load(completion: @escaping (Error) -> Void) {
+        self.client.get(from: self.url) { httpClientResult  in
+            switch httpClientResult {
+            case .failure:
+                completion(.connectivity)
+            case .sucess:
+                completion(.invalidData)
+            }
+        }
     }
 }
--- EDP/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EDP/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -18,17 +18,37 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
-        sut.load()
+        sut.load {_ in }
         XCTAssertEqual([url], client.requestedURLs)
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
-        sut.load()
-        sut.load()
+        sut.load {_ in }
+        sut.load {_ in }
         XCTAssertEqual([url, url], client.requestedURLs)
         XCTAssertEqual(2, client.requestedURLs.count)
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = self.makeSUT()
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        let clientError = NSError(domain: "Error", code: 0)
+        client.complete(with: clientError)
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+    
+    func test_load_deliversErrorOnNonHTTPResponse() {
+        let (sut, client) = self.makeSUT()
+        let invalidCodeSamples = [199, 201, 300, 400, 500]
+        invalidCodeSamples.enumerated().forEach { index, value in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            client.complete(withStatusCode: value, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     //   MARK: - Helpers
@@ -39,10 +59,25 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs =  [URL]()
+        private (set) var messages =  [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        var requestedURLs: [URL] {
+            return self.messages.map({ $0.url })
+        }
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+            self.messages.append((url, completion))
+        }
         
-        func get(from url: URL) {
-            self.requestedURLs.append(url)
+        func complete(with error: Error, at index: Int = 0) {
+            self.messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let url = self.requestedURLs[index]
+            let response = HTTPURLResponse(url: url,
+                                           statusCode: code,
+                                           httpVersion: nil,
+                                           headerFields: nil)!
+            self.messages[index].completion(.sucess(response))
         }
     }
 }
