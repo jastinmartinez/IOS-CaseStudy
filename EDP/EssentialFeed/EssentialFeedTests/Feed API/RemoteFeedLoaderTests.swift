a40fb8e2bd153c282888b326a2b88f181ab8506f
Jastin Martinez <martinez.jastin@hotmail.com>
2023-09-28 20:27:14 -0400

    test driven implementation for urlsession and include new target for ntn testing

--- EDP/EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EDP/EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -111,16 +111,6 @@ final class RemoteFeedLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private func trackForMemoryLeaks(instance: AnyObject,
-                                file: StaticString = #file,
-                                line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated, Potential memory leak.",
-                         file: file,
-                         line: line)
-        }
-    }
-    
     private func expect(_ sut: RemoteFeedLoader,
                         toCompleteWith expectedResult: RemoteFeedLoader.Result,
                         when action: () -> Void,
